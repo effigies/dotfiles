32c51857a0ffbf956417e2a3d1ff9f3fa98bca6a
Chris Markiewicz <effigies@gmail.com>
2024-12-18 08:36:49 -0500

    Add nvim plugins

---
+++ xdg_config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,1 @@
+return { "f-person/auto-dark-mode.nvim", opts = { update_interval = 200, fallback = "light" } }
---
+++ xdg_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+-- Setup mason and mason-lspconfig
+return {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup()
+    end,
+}
+    
