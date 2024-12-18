a662c329917f79fd6f31902b68481f6fe8ecb7db
Chris Markiewicz <effigies@gmail.com>
2024-12-18 08:34:18 -0500

    Add nvim config with lazy.lua

---
+++ xdg_config/nvim/init.lua
@@ -0,0 +1,12 @@
+-- Configuration is based on lazy.nvim
+-- See lua/config/settings.lua for basic settings
+require("config.settings")
+-- lazy loads lua/plugins/*.lua
+require("config.lazy")
+
+require('lspconfig').ruff.setup({
+    init_options = {
+        settings = {
+        }
+    }
+})
---
+++ xdg_config/nvim/lua/config/settings.lua
@@ -0,0 +1,20 @@
+-- Basic settings
+vim.o.number = true -- Enable line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.tabstop = 4 -- Number of spaces a tab represents
+vim.o.shiftwidth = 4 -- Number of spaces for each indentation
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.smartindent = true -- Automatically indent new lines
+vim.o.wrap = false -- Disable line wrapping
+vim.o.cursorline = true -- Highlight the current line
+vim.o.termguicolors = true -- Enable 24-bit RGB colors
+vim.o.formatoptions = tcqcro
+
+-- Syntax highlighting and filetype plugins
+vim.cmd('filetype plugin indent on')
+vim.cmd('syntax enable')
+
+-- Leader key
+vim.g.mapleader = ' ' -- Space as the leader key
+vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', { noremap = true, silent = true })
+
