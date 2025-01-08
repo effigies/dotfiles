7934c7477a9b528b78ed1544764bfe361c592067
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-01-07 22:27:58 -0500

    fix(nvim): Get cmp and copilot working better

--- xdg_config/nvim/lua/plugins/cmp.lua
+++ xdg_config/nvim/lua/plugins/cmp.lua
@@ -2,6 +2,7 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
+        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-buffer", -- source for text in buffer
         "hrsh7th/cmp-path", -- source for file system paths
         {
@@ -12,13 +13,26 @@ return {
         },
         "rafamadriz/friendly-snippets",
         "onsails/lspkind.nvim", -- vs-code like pictograms
+        "zbirenbaum/copilot-cmp",
     },
     config = function()
         local cmp = require("cmp")
         local lspkind = require("lspkind")
         local luasnip = require("luasnip")
+        local copilot_cmp = require("copilot_cmp")
 
         require("luasnip.loaders.from_vscode").lazy_load()
+
+        lspkind.init({
+            symbol_map = {
+                Copilot = "",
+            },
+        })
+        local has_words_before = function()
+          if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+        end
 
         cmp.setup({
             snippet = {
@@ -35,18 +49,50 @@ return {
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
                 }),
+                ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                  if cmp.visible() and has_words_before() then
+                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                  else
+                    fallback()
+                  end
+                end),
             }),
-            sources = cmp.config.sources({
+            sources = {
+                { name = "copilot" },
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
                 { name = "buffer" },
                 { name = "path" },
-            }),
+            },
+            sorting = {
+              priority_weight = 2,
+              comparators = {
+                require("copilot_cmp.comparators").prioritize,
+
+                -- Below is the default comparitor list and order for nvim-cmp
+                cmp.config.compare.offset,
+                -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+                cmp.config.compare.exact,
+                cmp.config.compare.score,
+                cmp.config.compare.recently_used,
+                cmp.config.compare.locality,
+                cmp.config.compare.kind,
+                cmp.config.compare.sort_text,
+                cmp.config.compare.length,
+                cmp.config.compare.order,
+              },
+            },
         })
 
         vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
+          set completeopt=menuone,noinsert,noselect
+          highlight! default link CmpItemKind CmpItemMenuDefault
+        ]])
+
+        -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+        -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+        --   capabilities = capabilities
+        -- }
     end,
 }
